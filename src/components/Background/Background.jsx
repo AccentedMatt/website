fe83d8ac42aa3c99aadfbb1f9ec9252250d3415e
Matthew Neilly <32590340+AccentedMatt@users.noreply.github.com>
2024-07-27 08:37:05 +0100

    Update of website
    Starting update using vite + react. Only initial stages.

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,8 @@
+import "./Background.css"
+import Image1 from "../../assets/igloocode_2023_helping.jpg"
+
+const Background = () => {
+    return <img src={Image1} className="background" alt="" />  
+}
+
+export default Background
